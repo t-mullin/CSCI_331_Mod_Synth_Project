a8973f821b43e1e95c0fef1574e050b4ea5517cc
Tristan <tristan.mullin1@gmail.com>
2021-10-24 22:21:33 -0600

    Added comments and added a global list of module objects.

--- js/audio.js
+++ js/audio.js
@@ -1,16 +1,17 @@
 //file for the Web Audio API integration
 const AudioContext = window.AudioContext || window.webkitAudioContext;
-const freqSquare = document.getElementById('freq_square');
-const freqSaw = document.getElementById('freq_saw');
-const freqSin = document.getElementById('freq_sin');
+//const freqSquare = document.getElementById('freq_square');
+//const freqSaw = document.getElementById('freq_saw');
+//const freqSin = document.getElementById('freq_sin');
 const playButton = document.getElementById('play');
 const pauseButton = document.getElementById('pause');
 const addButton = document.getElementById('add_module');
 const removeButton = document.getElementById('remove_module');
 let numOsc = 0;
+const rackArray = [];
 //const scope = document.getElementById('oscilloscope');
 //const scopeCtx = scope.getContext("2d");
-//const audioContext = new AudioContext();
+const audioContext = new AudioContext();
 
 //let osc_scope = audioContext.createAnalyser();
 //osc_scope.fftSize = 2048;
@@ -20,9 +21,15 @@ let numOsc = 0;
 
 //setup();
 
+/* add_osc_core: Adds the html containing the structure for a new oscillator module.
+ *               The module contains a squarewave, sawtoothwave, and sinewave oscillators.
+ *               The oscillators are intialized and events are added to sliders that control 
+ *               the frequency of the oscillators.   
+ */ 
 function add_osc_core() {
-    numOsc++;
-    let rack = document.getElementById('module_rack');
+    numOsc++; //used for template literals
+    let rack = document.getElementById('module_rack'); //gets the div that is going to contain the modules
+    //insert the html to make an oscillator module, each is unique due to using a template literal
     rack.insertAdjacentHTML('beforeend', 
         `<div id="osc_core_${numOsc}">
         <canvas id="oscilloscope"></canvas>
@@ -31,43 +38,70 @@ function add_osc_core() {
         <input id="freq_sin_${numOsc}" type="range" min="0" max="440" value="0" step="10">
         </div>`
     );
-
+    //add the module to the selector that selects the module for removal
     let remove_option = document.getElementById('remove_module_select');
     remove_option.insertAdjacentHTML('beforeend', `<option value="oscillators">Oscillator Pannel ${numOsc}</option>`);
 
+    //setting up the oscillatorNodes
+    let oscSquare = audioContext.createOscillator();
+    oscSquare.type = 'square';
+    oscSquare.frequency.value = 0;
+    let oscSaw = audioContext.createOscillator();
+    oscSaw.type = 'sawtooth';
+    oscSaw.frequency.value = 0;
+    let oscSine = audioContext.createOscillator();
+    oscSine.type = 'sine';
+    oscSine.frequency.value = 0;
+
+    //adding the oscillator module to the global list of modules
+    //NOTE: may need to change this to include the input/output to make connecting modules
+    //      easier
+    let oscillators = {id: `osc_core_${numOsc}`, osc1: oscSquare, osc2: oscSaw, osc3: oscSine}
+    rackArray.push(oscillators);
+    
+    //setting up event listeners for the frequency sliders
     document.getElementById(`freq_square_${numOsc}`).addEventListener('input', (e) => {
-        let newFreq = parseInt(e.target.value);
-        console.log(e.target);
-        //oscSquare.frequency.setValueAtTime(newFreq, audioContext.currentTime);
-        //console.log(oscSquare.frequency);
+        let newFreq = Number(e.target.value);
+        oscSquare.frequency.setValueAtTime(newFreq, audioContext.currentTime);
     }); 
 
     document.getElementById(`freq_saw_${numOsc}`).addEventListener('input', (e) => {
-        let newFreq = parseInt(e.target.value);
-        console.log(e.target);
-        //oscSaw.frequency.setValueAtTime(newFreq, audioContext.currentTime);
-        //console.log(oscSquare.frequency);
+        let newFreq = Number(e.target.value);
+        oscSaw.frequency.setValueAtTime(newFreq, audioContext.currentTime);
     });
 
     document.getElementById(`freq_sin_${numOsc}`).addEventListener('input', (e) => {
-        let newFreq = parseInt(e.target.value);
-        console.log(e.target);
-        //oscSine.frequency.setValueAtTime(newFreq, audioContext.currentTime);
-        //console.log(oscSquare.frequency);
+        let newFreq = Number(e.target.value);
+        oscSine.frequency.setValueAtTime(newFreq, audioContext.currentTime);
     });
 }
-
+/* remove_osc_core: This function removes the selected osc module from the DOM
+ *                  then removes it from the global module list. 
+ * NOTE: This will probably have to be modified to also disconnect any node attached to it.
+ */
 function remove_osc_core(selector) {
+    //gets the selected option from the selector element
     let module_name = selector.selectedOptions[0].innerHTML;
+    //gets the number from the selected element
     let module_num = module_name.match(/(\d+)/);
+    //gets the actual html by it's id
     let module = document.getElementById(`osc_core_${module_num[0]}`);
+    //gets the parent element
     let rack = document.getElementById('module_rack');
+    //removes the selected child element
     rack.removeChild(module);
+    //removes the option from the selector
     selector.selectedOptions[0].parentNode.removeChild(selector.selectedOptions[0]);
+    //removes the module from the global list of modules
+    let index = rackArray.findIndex(({id}) => id === `osc_core_${module_num[0]}`);
+    rackArray.splice(index, 1);
 }
 
+//sets up an event listener on the addButton
 addButton.addEventListener('click', () => {
+    //gets what module that the end user wants to add
     let selector = document.getElementById('add_module_select');
+    //selects the correct add module function based on the selected option 
     switch (selector.value) {
         case 'oscillators':
             add_osc_core();
@@ -77,8 +111,11 @@ addButton.addEventListener('click', () => {
     }
 });
 
+//sets up an event listener on the removeButton
 removeButton.addEventListener('click', () => {
+    //gets what module that the end user wants to remove
     let selector = document.getElementById('remove_module_select');
+    //selects the correct remove module function based on the selected option 
     switch (selector.value) {
         case 'oscillators':
             remove_osc_core(selector);
@@ -90,6 +127,7 @@ removeButton.addEventListener('click', () => {
 
 
 function setup() {
+    /*
     const oscSquare = audioContext.createOscillator();
     oscSquare.type = 'square';
     oscSquare.frequency = 0;
@@ -98,6 +136,7 @@ function setup() {
     oscSaw.frequency = 0;
     const oscSine = audioContext.createOscillator();
     oscSine.type = 'sine';
+    */
     const gain = audioContext.createGain();
     gain.gain.value = 0.5;
     oscSquare.connect(gain);
