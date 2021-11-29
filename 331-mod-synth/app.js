5e6963cf4eba42e665be9664dc4323831418c4a2
riley slater <rileymail6@gmail.com>
2021-11-28 20:32:39 -0700

    testing this server bis out

--- 331-mod-synth/app.js
+++ 331-mod-synth/app.js
@@ -20,21 +20,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  app.set('view engine', 'jade');
-  res.render('error');
-});
 
 module.exports = app;
+
+var MongoClient = require("mongodb").MongoClient;
+var url = "mongodb+srv://dbUser:dbPass@cluster0.h4at5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+
+MongoClient.connect(url, async function (err, client) {
+    var db= client.db('PresetDB');
+    if (err) throw err;
+    let presetsArr = await db.collection("Presets").find( { } ).toArray();
+    client.close();
+});
