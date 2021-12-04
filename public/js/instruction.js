8cbf3a16da096ffa8a6c5b65b89db658256d96f6
Riley Slater <rileymail6@gmail.com>
2021-12-03 17:37:46 -0700

    started instructions alert

---
+++ public/js/instruction.js
@@ -0,0 +1,13 @@
+
+
+
+function instructionsAlert() {
+    var osc = "The oscillators are circuts which produce continuous, repeating, alternating waveform.";
+    var lfo = `The LFO stands for Low Frequency Oscillator 
+            and this is functionally the same as a normal oscillator excpet 
+            typically the frequnecy is usually below 20 Hz.`;
+    var dist = "The distortion module is responsible for making alterations to the waveform created by the oscillator.";
+    var filter = "";
+    var vca = "";
+    
+}
